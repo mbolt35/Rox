5a0dd52a9b51ab23873084004def0dfb0a2bf90b
Matt Bolt <mbolt35@gmail.com>
2017-06-15 10:23:12 -0400

    Day 13; Final adjustments.

--- Rox/Rox/Voxel/BlockType.cs
+++ Rox/Rox/Voxel/BlockType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rox.Voxel {
+﻿namespace Rox.Voxel {
     
     /// <summary>
     /// The type of block/voxel. 
